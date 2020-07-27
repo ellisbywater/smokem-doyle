c215b8843388db4890b5621a2148a9000e4e05c6
Ellis G <egaraudy@gmail.com>
2020-07-27 16:12:56 -0600

    create player movement and corresponding camera trailing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5f,
+                 gravityModifier = 1,
+                 jumpPower = 3,
+                 runSpeed = 2;
+    
+    public CharacterController characterController;
+    public Transform cameraTransform;
+
+    private Vector3 moveInput;
+
+    public float mouseSensitivity = 2f;
+    public bool invertX;
+    public bool invertY;
+
+    private bool canJump, canDoubleJump;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+    
     
     // Start is called before the first frame update
     void Start()
@@ -14,6 +32,66 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //moveInput.x = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        //moveInput.y = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         
+        //store y velocity
+        float yStore = moveInput.y;
+
+        Vector3 vertMove = transform.forward * Input.GetAxis("Vertical");
+        Vector3 horzMove = transform.right * Input.GetAxis("Horizontal");
+
+        moveInput = horzMove + vertMove;
+        moveInput.Normalize();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            moveInput = moveInput * runSpeed;
+        }
+        else
+        {
+            moveInput *= moveSpeed;
+        }
+
+        moveInput.y = yStore;
+        moveInput.y += Physics.gravity.y * gravityModifier * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            moveInput.y = Physics.gravity.y * gravityModifier * Time.deltaTime;
+        }
+
+        canJump = Physics.OverlapSphere(groundCheckPoint.position, .25f, whatIsGround).Length > 0;
+
+        if (canJump)
+        {
+            canDoubleJump = false;
+        }
+        
+        // Handle Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            moveInput.y = jumpPower;
+        } else if (Input.GetKeyDown(KeyCode.Space) && canDoubleJump)
+        {
+            moveInput.y = jumpPower;
+            canDoubleJump = false;
+        }
+        characterController.Move(moveInput * Time.deltaTime);
+        
+        // Control Camera Rotation
+        Vector2  mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        if (invertX)
+        {
+            mouseInput.x = -mouseInput.x;
+        }
+
+        if (invertY)
+        {
+            mouseInput.y = -mouseInput.y;
+        }
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x,transform.rotation.eulerAngles.z );
+        cameraTransform.rotation = Quaternion.Euler(cameraTransform.rotation.eulerAngles + new Vector3(-mouseInput.y, 0f, 0f));
     }
 }
